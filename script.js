900c23299145e5830af5efa73e5e44cafd075ee8
fedor0612 <tayger789@yandex.by>
2023-05-15 20:49:30 +0300

    o

---
+++ script.js
@@ -0,0 +1,18 @@
+// const calculator = {
+//     val1: 0,
+//     val2: 0
+// }
+
+// // calculator.val1 = 5;
+// calculator.val2 = 2;
+
+
+// console.log(calculator.val1);
+// document.writeln(calculator.val1)
+
+// const obj1 = {a:5};
+// const obj2 = {b:6};
+// const obj3 = {c:7};
+// obj1.__proto__=
+
+
